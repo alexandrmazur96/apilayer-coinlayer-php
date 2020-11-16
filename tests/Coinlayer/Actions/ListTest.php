3cf6e259b9d9dabbc16297c11961b4fe2ef658c7
Alexandr Mazur <amazur@xmlshop.com>
2020-11-17 00:36:39 +0200

    Added tests for List action

---
+++ tests/Coinlayer/Actions/ListTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Apilayer\Tests\Coinlayer\Actions;
+
+use Apilayer\Coinlayer\Actions\ActionInterface;
+use Apilayer\Coinlayer\Actions\ListAction;
+use Apilayer\Tests\TestCase;
+
+class ListTest extends TestCase
+{
+    public function testGetEndpoint(): void
+    {
+        $listAction = new ListAction(null);
+        self::assertEquals(ActionInterface::ENDPOINT_LIST, $listAction->getEndpoint());
+    }
+
+    public function testGetData(): void
+    {
+        $listAction = new ListAction(null);
+        self::assertEquals([], $listAction->getData());
+
+        $listAction = new ListAction('some_callback');
+        self::assertEquals(['callback' => 'some_callback'], $listAction->getData());
+    }
+}
