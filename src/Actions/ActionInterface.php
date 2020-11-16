4a910788e652819c8ed0faafa8f16f5af3b4bbdd
Alexandr Mazur <amazur@xmlshop.com>
2020-11-16 23:59:54 +0200

    Removed HISTORICAL_ENDPOINT constant from ActionInterface.php because it's unused

--- src/Actions/ActionInterface.php
+++ src/Actions/ActionInterface.php
@@ -7,7 +7,6 @@ use Apilayer\Coinlayer\Responses\Factories\ResponseFactoryInterface;
 interface ActionInterface
 {
     public const ENDPOINT_LIVE = 'live';
-    public const ENDPOINT_HISTORICAL = 'historical';
     public const ENDPOINT_CONVERT = 'convert';
     public const ENDPOINT_TIMEFRAME = 'timeframe';
     public const ENDPOINT_CHANGE = 'change';
